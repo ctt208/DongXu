d6910e8ce2cb03245799d8f6a088738f2e183592
2395073949 <2395073949@qq.com>
2019-01-04 15:32:49 +0800

    QYG

---
+++ DongXu/DongXu.Entity/Bloc.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DongXu.Entity
+{
+    /// <summary>
+    /// 集团表
+    /// </summary>
+    public class Bloc
+    {
+        /// <summary>
+        /// 主键ID
+        /// </summary>
+        public int BlocID { get; set; }
+        /// <summary>
+        /// 集团名称
+        /// </summary>
+        public string BlocName { get; set; }
+        /// <summary>
+        /// PID
+        /// </summary>
+        public int PID { get; set; }
+        /// <summary>
+        /// 是否启用
+        /// </summary>
+        public int IsOrNo { get; set; }
+    }
+}
---
+++ DongXu/DongXu.Entity/TarGetType.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DongXu.Entity
+{
+    /// <summary>
+    /// 目标类别表
+    /// </summary>
+    public class TarGetType
+    {
+        /// <summary>
+        /// 主键ID
+        /// </summary>
+        public int TarGetTypeID { get; set; }
+        /// <summary>
+        /// 目标类别名称
+        /// </summary>
+        public string TarGetTypeName { get; set; }
+        /// <summary>
+        /// PID
+        /// </summary>
+        public int PID { get; set; }
+        /// <summary>
+        /// 是否启用
+        /// </summary>
+        public int IsOrNo { get; set; }
+    }
+}
