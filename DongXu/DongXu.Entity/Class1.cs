17d4de70c18362cbc29eb5640d1ed6e5bdeaaa55
ctt <18524798208@163.com>
2019-01-04 10:29:03 +0800

    zjc

---
+++ DongXu/DongXu.Entity/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DongXu.Entity
+{
+    public class Class1
+    {
+    }
+}
---
+++ DongXu/DongXu.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DongXu.MVC.MvcApplication" Language="C#" %>
