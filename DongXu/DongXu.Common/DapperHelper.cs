db3553fc7777afe729655b69e6e52eef7ecc3b5e
ctt <18524798208@163.com>
2019-01-04 10:56:39 +0800

    zjc

---
+++ DongXu/DongXu.Common/DapperHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DongXu.Common
+{
+    using System.Configuration;
+    using Oracle.ManagedDataAccess.Client;
+    public class DapperHelper
+    {
+        private static string oracleConnectionString = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
+        public static OracleConnection GetConnectionString()
+        {
+            return new OracleConnection(oracleConnectionString);
+        }
+    }
+}
