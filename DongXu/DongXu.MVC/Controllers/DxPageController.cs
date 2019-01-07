a985bea35062e747f2bf851d7a9f4c26c2085ca7
jw6666 <45486238+jw6666@users.noreply.github.com>
2019-01-07 09:48:13 +0800

    jw

---
+++ DongXu/DongXu.MVC/Controllers/DxPageController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DongXu.MVC.Controllers
+{
+    public class DxPageController : Controller
+    {
+        // GET: DxPage
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Backlog()
+        {
+            return View();
+        }
+    }
+}
