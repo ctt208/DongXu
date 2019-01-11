8022a33ae6e7cb8498f7126f670ff87a02da60fe
ctt <18524798208@163.com>
2019-01-11 13:40:05 +0800

    zjc

---
+++ DongXu/DongXu.MVC/Controllers/TargetController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DongXu.MVC.Controllers
+{
+    public class TargetController : Controller
+    {
+        // GET: Target
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
