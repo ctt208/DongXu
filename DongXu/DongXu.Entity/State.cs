defc73a349cb2a716c1b4c0850b94b9f66e24934
2395073949 <2395073949@qq.com>
2019-01-15 20:25:15 +0800

    QYG

--- DongXu/DongXu.Entity/State.cs
+++ DongXu/DongXu.Entity/State.cs
@@ -8,11 +8,33 @@ namespace DongXu.Entity
 {
     public class State
     {
+        /// <summary>
+        /// 主键
+        /// </summary>
         public int StateID { get; set; }
+        /// <summary>
+        /// 名称
+        /// </summary>
         public string StateName { get; set; }
+        /// <summary>
+        /// 级别
+        /// </summary>
         public string StateJB { get; set; }
-        public string StateRed { get; set; }
-        public string StateYellow { get; set; }
-        public string StateGrenn { get; set; }
+        /// <summary>
+        /// 红灯
+        /// </summary>
+        public int StateRed { get; set; }
+        /// <summary>
+        /// 黄灯
+        /// </summary>
+        public int StateYellow { get; set; }
+        /// <summary>
+        /// 绿灯
+        /// </summary>
+        public int StateGrenn { get; set; }
+        /// <summary>
+        /// 积分
+        /// </summary>
+        public int StateFen { get; set; }
     }
 }
