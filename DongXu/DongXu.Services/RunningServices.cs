ef65d2eea62f1db613bd8c6a8de5ce9b1f1fe84c
jw6666 <45486238+jw6666@users.noreply.github.com>
2019-01-19 08:31:49 +0800

    jw

--- DongXu/DongXu.Services/RunningServices.cs
+++ DongXu/DongXu.Services/RunningServices.cs
@@ -22,7 +22,7 @@ namespace DongXu.Services
             using (OracleConnection conn = DapperHelper.GetConnectionString())
             {
                 conn.Open();
-                string sql = @"select b.name,a.numberids,a.per from (select statusid,count(1) as numberids,TRUNC(count(1)/(select count(1)  from targetdetails )*100,2) as per from targetdetails group by statusid)  a inner join status b on a.statusid = b.id";
+                string sql = @"select b.name,a.numberids,a.per from (select statusid,count(1) as numberids,TRUNC(count(1)/(select count(1)  from targetdetails a inner join target b on a.targetid=b.id)*100,2) as per from targetdetails group by statusid)  a inner join status b on a.statusid = b.id";
                 IEnumerable<TargetDetails> targetDetailslist = conn.Query<TargetDetails>(sql, null);
                 return targetDetailslist.ToList();
             }
